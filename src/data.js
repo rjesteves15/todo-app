499b308ba02d78abb561cf332582093940e68b9c
rchllstsvs <rochellejoan.p.esteves@gmail.com>
2022-10-14 22:37:43 +0800

    added todo list data

---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const data = [
+    {
+      id: 1,
+      task: "Team Meeting",
+      location: "Room 404",
+      time: "10:20AM"
+    },
+    {
+      id: 2,
+      task: "Check Email",
+      location: "Room 404",
+      time: "10:20AM"
+    }
+  ];
+  
